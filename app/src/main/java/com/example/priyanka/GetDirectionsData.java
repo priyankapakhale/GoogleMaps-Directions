d0b58b343b01f365df3fa4f91b72dbf3fa3a3e24
Priyanka Pakhale <ppakhale@asu.edu>
2017-08-16 17:14:05 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/priyanka/GetDirectionsData.java
@@ -0,0 +1,80 @@
+package com.example.priyanka.mapsnearbyplaces;
+
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
+
+import java.io.IOException;
+import java.nio.DoubleBuffer;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @auth Priyanka
+ */
+
+public class GetDirectionsData extends AsyncTask<Object,String,String> {
+
+    GoogleMap mMap;
+    String url;
+    String googleDirectionsData;
+    String duration, distance;
+    LatLng latLng;
+    @Override
+    protected String doInBackground(Object... objects) {
+        mMap = (GoogleMap)objects[0];
+        url = (String)objects[1];
+        latLng = (LatLng)objects[2];
+
+
+
+        DownloadUrl downloadUrl = new DownloadUrl();
+        try {
+            googleDirectionsData = downloadUrl.readUrl(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return googleDirectionsData;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+
+        String[] directionsList;
+        DataParser parser = new DataParser();
+        directionsList = parser.parseDirections(s);
+        displayDirection(directionsList);
+
+    }
+
+    public void displayDirection(String[] directionsList)
+    {
+
+        int count = directionsList.length;
+        for(int i = 0;i<count;i++)
+        {
+            PolylineOptions options = new PolylineOptions();
+            options.color(Color.RED);
+            options.width(10);
+            options.addAll(PolyUtil.decode(directionsList[i]));
+
+            mMap.addPolyline(options);
+        }
+    }
+
+
+
+
+
+
+}
+
